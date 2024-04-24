1d5d32ea0c7df424426b98baffa7ee4ee196838a
Khandaker Samin Yeasar <138380913+KhandakerSamin@users.noreply.github.com>
2024-04-24 23:41:07 +0600

    funtion

---
+++ function.js
@@ -0,0 +1,20 @@
+const number = 45;
+function fanOffKor(){
+    console.log("Fan off korte hobe ");
+}
+
+fanOffKor();
+
+
+function square (number ) {
+    console.log(number)
+    console.log("Square answer", number*number)
+}
+
+function areaOfTriangle (a,b) {
+    const area = 0.5*a*b ;
+    console.log("Area of your tiangle is : ", area )
+}
+
+square(40);
+areaOfTriangle(4,5)
